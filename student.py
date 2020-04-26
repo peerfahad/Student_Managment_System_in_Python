cf37f0e5aeb4a31630b639b204327868c8abf82a
peerfahad <fahadrashid611@gmail.com>
2020-04-27 01:28:15 +0500

    first commit

---
+++ student.py
@@ -0,0 +1,14 @@
+from tkinter import*
+class student:
+    def __init__(self,root):
+      self.root=root
+      self.root.tittle("Student Management System")
+      self.root.geometry(""1350x700+0+0)
+
+
+
+root=Tk()
+ob=student(root)
+root.mainloop()
+
+
