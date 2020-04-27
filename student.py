2c10281ebce5366eb7a3e448d89b37ca7037f4fc
peerfahad <fahadrashid611@gmail.com>
2020-04-27 18:25:25 +0500

    Making the frames

--- student.py
+++ student.py
@@ -6,8 +6,14 @@ class student:
       self.root.title("Student Management System")
       self.root.geometry("1350x700+0+0")
 
-      title=Label(self.root,text="Student Managemnt System",font=("time new roman",40,"bold"),bg="yellow",fg="red")
-      title.pack(side=TOP)
+      title=Label(self.root,text="Student Managemnt System",bd=10,relief=GROOVE,font=("time new roman",40,"bold"),bg="yellow",fg="red")
+      title.pack(side=TOP,fill=X)
+
+      Manage_frame = Frame(self.root,bd=4,relief=RIDGE,bg="crimson")
+      Manage_frame.place(x=20,y=90,width=350,height=560)
+
+      detail_frame = Frame(self.root,bd=4,relief=RIDGE,bg="crimson")
+      detail_frame.place(x=500,y=90,width=750,height=560)
 
 root=Tk()
 ob=student(root)
