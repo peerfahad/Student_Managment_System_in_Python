691ccce0dbd6fafa7de4e1b28771480a32c27112
peerfahad <fahadrashid611@gmail.com>
2020-04-27 18:15:28 +0500

    Adding label

--- student.py
+++ student.py
@@ -1,11 +1,13 @@
 from tkinter import*
+from PIL import ImageTk
 class student:
     def __init__(self,root):
       self.root=root
-      self.root.tittle("Student Management System")
-      self.root.geometry(""1350x700+0+0)
+      self.root.title("Student Management System")
+      self.root.geometry("1350x700+0+0")
 
-
+      title=Label(self.root,text="Student Managemnt System",font=("time new roman",40,"bold"),bg="yellow",fg="red")
+      title.pack(side=TOP)
 
 root=Tk()
 ob=student(root)
